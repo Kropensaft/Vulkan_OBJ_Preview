51efcf9daa4a0fd14c96b8bdc5bf570901a9eb78
Kropensaft <vojta.vlachovsky@gmail.com>
2026-02-18 16:10:35 +0100

    chore: flush git cache and apply new gitignore rules

--- build/CMakeFiles/VulkanApp.dir/src/platforms/linux/menu_linux.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/VulkanApp.dir/src/platforms/linux/menu_linux.cpp.o: \
-  /Volumes/T7\ Shield/Vulkan/Vulkan_OBJ_Preview/src/platforms/linux/menu_linux.cpp
--- build/CMakeFiles/VulkanApp.dir/src/platforms/windows/menu_win.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/VulkanApp.dir/src/platforms/windows/menu_win.cpp.o: \
-  /Volumes/T7\ Shield/Vulkan/Vulkan_OBJ_Preview/src/platforms/windows/menu_win.cpp
