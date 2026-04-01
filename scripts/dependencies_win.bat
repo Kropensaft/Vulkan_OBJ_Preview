570691e0eb9e98a2121bbf91628b8b0250152131
Kropensaft <vojta.vlachovsky@gmail.com>
2026-04-01 14:50:13 +0200

    modify bash script for win11

--- scripts/dependencies_win.bat
+++ scripts/dependencies_win.bat
@@ -18,6 +18,8 @@ if %ERRORLEVEL% equ 0 (
 ) else (
     echo [ERROR] vcpkg not found.
     echo Please git clone https://github.com/Microsoft/vcpkg.git and run .\bootstrap-vcpkg.bat
+    echo After installing you have to add the folder path to 'System Environment variables' under the key PATH
+    echo This can be done via the Windows GUI when searching for System Environment
     exit /b 1
 )
 
@@ -35,7 +37,9 @@ echo.
 echo Don't forget to install the Vulkan SDK from https://vulkan.lunarg.com/
 echo.
 echo All dependencies installed!
-echo To build, use:
+echo To build, replace [path\to\vcpkg] with the path where you installed it and run this:
 echo cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=[path\to\vcpkg]\scripts\buildsystems\vcpkg.cmake
-
+echo after this successfully completes run: 
+echo cmake --build build
+echo the application will be inside .\build\VulkanApplication.exe
 exit /b 0
